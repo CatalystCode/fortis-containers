ae544acbca2c51055a2225cd41ceec15b76d455f
erisch <erik.schlegel@gmail.com>
2017-08-17 12:43:39 -0700

    Integrating azure file storage PVC mount to fortis deployment.

--- ops/create-cluster.sh
+++ ops/create-cluster.sh
@@ -74,7 +74,8 @@ echo "Finished. Deploying environment settings to cluster."
     "${eh_conn_str}" \
     "${feature_service_db_conn_str}" \
     "${fortis_central_directory}" \
-    "${sb_conn_str}"
+    "${sb_conn_str}" \
+    "${storage_account_name}"
 
 echo "Finished. Installing spark cluster."
 ./install-spark.sh "${k8spark_worker_count}" "${spark_config_map_name}" "${fortis_central_directory}" "${storage_account_name}" "${storage_account_key}" "${checkpointfileshare}"
